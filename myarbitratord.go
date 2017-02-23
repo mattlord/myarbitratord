0e5f740c0a60ef98ec29576c99a70db03ce7c949
Matt Lord <matt.lord@oracle.com>
2017-02-23 07:58:15 -0500

    Improved log message ordering

--- myarbitratord.go
+++ myarbitratord.go
@@ -267,13 +267,13 @@ func MonitorCluster( seed_node group.Node ) error {
           err = last_view[i].Connect()
           defer last_view[i].Cleanup()
 
+          InfoLog.Printf( "Determining if we should shut down potentially non-healthy node: '%s:%s'\n", last_view[i].Mysql_host, last_view[i].Mysql_port )
+
           if( err == nil ){
             quorum, err = last_view[i].HasQuorum()
 
+            // If this node sees itself in the ERROR state or doesn't think it has a quorum, then it should be safe to shut it down
             if( last_view[i].Member_state == "ERROR" || quorum == false ){
-              InfoLog.Printf( "Determining if we should shut down potentially non-healthy node: '%s:%s'\n", last_view[i].Mysql_host, last_view[i].Mysql_port )
-
-              // If this node lists itself as in the ERROR state or doesn't think it has a quorum, then it should be safe to shut it down
               InfoLog.Printf( "Shutting down non-healthy node: '%s:%s'\n", last_view[i].Mysql_host, last_view[i].Mysql_port )
               err = last_view[i].Shutdown()
             }
